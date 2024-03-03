ecfe1497aef017878356ce6c76564e0c8b0a74b1
200585757 <156979247+200585757@users.noreply.github.com>
2024-03-02 22:49:01 -0500

    Add files via upload

---
+++ includes/footer-nav.php
@@ -0,0 +1,5 @@
+            <nav>
+				<a href="./about/privacy-policy.php" title="Read our Privacy Policy" target="_main">Privacy Policy</a> | <a href="./about/privacy-policy.php#personal-information" title="Learn about how we treat your personal information" target="_main">Use of Personal Information</a> 
+                | <a href="./about/terms-of-use.php" title="Understand our Terms of Use" target="_main">Terms of Use</a> | <a href="./about/terms-of-use.php#limitation-liability" title="Our Limitation of Liability explained" target="_main">Limitation of Liability</a>
+			</nav>
+			<p><small>© HTML, CSS, and JS Blocks Inc.</small></p>
